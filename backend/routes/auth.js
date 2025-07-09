8f00d53dfa013fcad037ad47dff9a965e1d7588c
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-07-09 09:36:02 +0200

    Remove register route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,18 +5,6 @@ import User from "../models/User.js";
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  try {
-    const user = await User.create({ username, password: hashedPassword });
-    res.status(201).json({ message: "User created" });
-  } catch (err) {
-    res.status(400).json({ error: "User already exists" });
-  }
-});
-
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
